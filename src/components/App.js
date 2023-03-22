166302f7a70961d3b9bd2a8ae610347d62837f91
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-22 10:20:45 +0530

    Commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar";
+import Moviecard from "./Moviecard";
+import { data } from "../data.js";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movie</div>
+          <div className="tab">Favourite</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => {
+            return <Moviecard movie={movie} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
