88f67af17b882b3f6e5389d3314b694cf31ea8f5
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-25 13:57:01 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/App';
 
+const store = createS
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
