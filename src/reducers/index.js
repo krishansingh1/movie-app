6a5d61cf86ef78f88bb29dcba9bafd58701bb039
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-25 21:15:00 +0530

    Commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,15 +6,6 @@ const initialMoviesState = {
 }
 
 export default function movies(state = initialMoviesState, action) {
-    // if (action.type === ADD_MOVIES) {
-    //     return {
-    //         ...state,
-    //         list: action.movies,
-    //     };
-    // }
-
-    // return state;
-
     switch (action.type) {
         case ADD_MOVIES:
             return {
