132bbfd77f790ef3c85bd0aed88eb4fad8aee401
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-25 20:16:09 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import Navbar from "./Navbar";
 import Moviecard from "./Moviecard";
+import { useEffect } from "react";
+import { data } from '../data';
 
 
 export default function App(props) {
+    useEffect(() => {
+        props.store.dispatch({
+            type: "ADD_MOVIES",
+            movies: data,
+        })
+        console.log(props.store.getState());
+    }, [props])
+
     const movies = props.store.getState();
+
     return (
         <>
             <Navbar />
