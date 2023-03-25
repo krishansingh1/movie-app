93973210162de5c83e74d5c54db578f34c054de3
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-25 20:22:47 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,45 @@
 import Navbar from "./Navbar";
 import Moviecard from "./Moviecard";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { data } from '../data';
 
 
-export default function App(props) {
-    useEffect(() => {
-        props.store.dispatch({
+class App extends React.Component {
+    componentDidMount() {
+        const { store } = this.props;
+        store.subscribe(() => {
+            console.log("Updated");
+            this.forceUpdate();
+        })
+
+        store.dispatch({
             type: "ADD_MOVIES",
             movies: data,
         })
         console.log(props.store.getState());
-    }, [props])
+    }
 
-    const movies = props.store.getState();
+    render() {
+        const movies = props.store.getState();
 
-    return (
-        <>
-            <Navbar />
-            <div className="main">
-                <div className="tabs">
-                    <div className="tab">Movie</div>
-                    <div className="tab">Favourite</div>
+        return (
+            <>
+                <Navbar />
+                <div className="main">
+                    <div className="tabs">
+                        <div className="tab">Movie</div>
+                        <div className="tab">Favourite</div>
+                    </div>
+                    <div className="list">
+                        {movies.map((movie, index) => {
+                            return <Moviecard movie={movie} key={index} />;
+                        })}
+                    </div>
                 </div>
-                <div className="list">
-                    {movies.map((movie, index) => {
-                        return <Moviecard movie={movie} key={index} />;
-                    })}
-                </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 }
+
+
+export default App;
