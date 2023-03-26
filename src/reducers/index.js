ecffcf10331bbc6a1ff860f2ed1589f846d4a4f4
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-26 22:32:16 +0530

    Commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,15 @@ const initialMoviesState = {
     showFavourites: false,
 }
 
+const logger = function ({ dispatch, getState }) {
+    return function (next) {
+        return function (action) {
+            console.log("ACTION_TYPE= ", action.type);
+            next(action);
+        }
+    }
+}
+
 export function movies(state = initialMoviesState, action) {
     switch (action.type) {
         case ADD_MOVIES:
