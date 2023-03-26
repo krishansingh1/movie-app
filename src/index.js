e59060b018dccae93c451d926cd538343cc8cf47
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-27 00:40:42 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from '../src/components/App';
@@ -16,17 +17,19 @@ import rootReducer from './reducers';
 // }
 
 const logger = ({ dispatch, getState }) => (next) => (action) => {
-  // console.log("ACTION_TYPE= ", action.type);
-  next(action);
-}
-
-const thunk = ({ dispatch, getState }) => (next) => (action) => {
-  if (typeof action === 'function') {
-    action(dispatch);
-    return;
+  if (typeof action !== 'function') {
+    console.log("ACTION_TYPE= ", action.type);
   }
   next(action);
 }
+
+// const thunk = ({ dispatch, getState }) => (next) => (action) => {
+//   if (typeof action === 'function') {
+//     action(dispatch);
+//     return;
+//   }
+//   next(action);
+// }
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
