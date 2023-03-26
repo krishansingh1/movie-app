e1010c7d7d3def60f4771220171f9ed211f92e91
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-27 00:53:13 +0530

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showSearchResults: true,
+            showSearchResults: false,
             searchText: "",
         }
     }
