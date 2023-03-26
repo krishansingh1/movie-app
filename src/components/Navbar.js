aca20b4b6820896e0889be2271bdd8b6788c5577
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-26 23:42:45 +0530

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,12 +11,24 @@ class Navbar extends React.Component {
         }
     }
 
+    handleAddToMovies = (movie) => {
+        this.props.dispatch(addMovieToList(movie));
+        this.setState({
+            showSearchResults: false,
+        })
+    }
+
+    handleSearch = () => {
+
+    }
+
     render() {
+        const { showSearchResults } = this.state;
         return (
             <div className="nav">
                 <div className="search-container">
                     <input />
-                    <button id="search-btn">Search</button>
+                    <button id="search-btn" onClick={this.handleSearch}>Search</button>
                 </div>
             </div>
         );
